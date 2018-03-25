a1625cfb0565a05b74f1bc0649ae7bfa4da42aed
Phebus <phebust@dca90482b817.ant.amazon.com>
2018-03-25 14:29:34 -0500

    Extracted mission statements

--- mission_statmenet.py
+++ mission_statmenet.py
@@ -1,12 +1,20 @@
 import pickle
+import re
 found=set()
 pages = pickle.load(open("501c.p","rb"))
 for cat in pages.keys():
     for key in pages[cat].keys():
+        lines_left = 0
         for line in pages[cat][key]:
-            if 'mission' in line.lower():
-                print cat+"|"+key+":"+line.strip()
+            
+            if re.match('.*\Wmission\W.*',line.lower()):
+                lines_left = 6
+                if key not in found:
+                    print ""
                 found.add(key)
+            if lines_left >0:
+                print cat+"|"+key+":"+line.strip()
+                lines_left-=1
 print found
 print len(found)
 
